504ae3cb3a06b328b275ba172139d15524abc866
eswar916 <eswaralahari916@gmail.com>
2025-03-01 17:31:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+
+app.set("view engine", "ejs");
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const home1 = ["Luxury homes", 3050, "4bhk residential",'images/lux1.jpeg', 'images/lux2.jpeg','images/lux3.jpeg', 'images/lux4.jpeg'];
+const home2 = ["Modern homes", 2750, "3bhk residential",'images/mod1.jpeg', 'images/mod2.jpeg','images/mod3.jpeg', 'images/mod4.jpeg'];
+const home3 = ["Pakka homes", 2270, "2bhk residential", 'images/ren1.jpeg', 'images/ren2.jpeg','images/ren3.jpeg', 'images/ren4.jpeg'];
+
+const format1 = {};
+format1.home1 = home1;
+format1.home2 = home2;
+format1.home3 = home3;
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.post("/lux", (req, res) => {
+    res.render("ind", { data: format1.home1 });
+});
+
+app.post("/mod", (req, res) => {
+    res.render("ind", { data: format1.home2 });
+});
+
+app.post("/ren", (req, res) => {
+    res.render("ind", { data: format1.home3 });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
